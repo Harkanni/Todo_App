ef7b5eac696ff1e7a4a41e2a92c8e78eecb36358
harkanni <harkanzyempire001@gmail.com>
2023-07-04 12:58:37 +0100

    conditional rendering using ternary operation

--- src/components/ConditionalComponent.js
+++ src/components/ConditionalComponent.js
@@ -1,13 +1,10 @@
 import { useState } from "react"
 
 export default function ConditionalComponent() {
-    const [display, setDisplay] = useState(true)
-    let output
-    if(display){
-        output = <h3>This is a conditional component</h3>
-    } else {
-        output = <h3>Nothing to see here</h3>
-    }
+    const [display, setDisplay] = useState(false)
+    
+    return display ? <div><h3>This is a conditional component</h3></div> : <h3>Nothing to see here</h3>
 
-    return <div>{output}</div>
+
+
 }

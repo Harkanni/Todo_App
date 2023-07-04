5008b30c526b64a5dfa520c01398f2dbe915b60b
harkanni <harkanzyempire001@gmail.com>
2023-07-04 18:24:19 +0100

    Renderring Array

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,6 +1,23 @@
 export default function Product () {
-    const products = ["Laptop", "IPhone", "Modem", "Computer"]
-    const productList = products.map((product) => (<h3>{product}</h3>
+    const products = [
+        { id: 1, name: "Laptop", price: "$500"},
+        { id: 2, name: "Phone", price: "$200"},
+        { id: 3, name: "Modem", price: "$50"},
+        { id: 4, name: "Laptop", price: "$900"}
+    ]
+
+    const fruits = ["Apple", "Mango", "Banana"]
+    const fruitList = fruits.map((fruit, index) => (
+        <h3 key={index}>{ fruit }</h3>
     ))
-    return (<div> {productList} </div>)
+
+    const productList = products.map((product) => (
+        <h3 key={product.id}> {product.name}: {product.price} </h3>
+    ))
+
+    return <div>
+        { productList }
+
+        { fruitList }
+    </div>
 }

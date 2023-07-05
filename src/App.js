b5b2f093aa852ab9ba0fb417b98a4de3acc2c696
harkanni <harkanzyempire001@gmail.com>
2023-07-05 20:27:01 +0100

    css modules in react

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import ModuleComponent from './components/ModuleComponent';
 import StylesheetComponent from './components/StylesheetComponent';
 
 function App() {
   return (
     <div className="App">
+      <ModuleComponent></ModuleComponent>
       <StylesheetComponent></StylesheetComponent>
     </div>
   );

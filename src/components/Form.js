13b9e7355d4a4514629067dd01604a49fb21bb78
harkanni <harkanzyempire001@gmail.com>
2023-07-05 11:44:18 +0100

    Handling User Input:2

--- src/components/Form.js
+++ src/components/Form.js
@@ -3,10 +3,17 @@ import {Component} from "react"
 class Form extends Component {
     state = {
         firstname: "",
+        lastname: "",
     }
     handleChange(event){
         this.setState({
             firstname: event.target.value
+        })
+    }
+
+    handleLastNameChange(event){
+        this.setState({
+            lastname: event.target.value
         })
     }
     render() {
@@ -15,6 +22,7 @@ class Form extends Component {
                 <h2>Form</h2>
                 <form>
                     <input onChange={(event) => this.handleChange(event)} value={this.state.firstname} type="text"></input>
+                    <input onChange={(event) => this.handleLastNameChange(event)} type="text" value={this.state.lastname}></input>
                 </form>
             </div>
         )

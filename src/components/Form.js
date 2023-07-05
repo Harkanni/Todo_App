3fbfdc5f5fce4f964313ed04b7d3bf09b0a9783d
harkanni <harkanzyempire001@gmail.com>
2023-07-05 11:51:49 +0100

    Form submit

--- src/components/Form.js
+++ src/components/Form.js
@@ -16,13 +16,22 @@ class Form extends Component {
             lastname: event.target.value
         })
     }
+
+    handleSubmit(event){
+        event.preventDefault()
+        console.log({
+            fname: this.state.firstname,
+            lname: this.state.lastname
+        })
+    }
     render() {
         return (
             <div> 
                 <h2>Form</h2>
-                <form>
+                <form onSubmit={(event) => this.handleSubmit(event)}>
                     <input onChange={(event) => this.handleChange(event)} value={this.state.firstname} type="text"></input>
                     <input onChange={(event) => this.handleLastNameChange(event)} type="text" value={this.state.lastname}></input>
+                    <button type="submit">Submit</button>
                 </form>
             </div>
         )
